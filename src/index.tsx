6345524bf4167dd1dae9b0cb236d2bf8eb3d1390
unixetp <iarepetsky@outlook.com>
2026-02-07 11:11:49 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { plugin } from "@vendetta";
+import { findByProps } from "@vendetta/metro";
+import { toSmallCaps } from "./smallcaps";
+
+const MessageActions = findByProps("sendMessage");
+
+export default {
+  onLoad() {
+    this.unpatch = plugin.after(
+      MessageActions,
+      "sendMessage",
+      (args) => {
+        const [channelId, message] = args;
+        if (message.content.startsWith("/yaica ")) {
+          message.content = toSmallCaps(message.content.slice(4));
+        }
+      }
+    );
+  },
+  onUnload() {
+    this.unpatch?.();
+  }
+};
